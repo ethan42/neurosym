97151b05ce7b3490d9806bdc5b09cdda39012245
Thanassis Avgerinos <thanassis.avgerinos@gmail.com>
2024-12-05 10:05:55 -0500

    Add a toy Z3tool.

--- neurosym/__init__.py
+++ neurosym/__init__.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, field
 from dataclasses_jsonschema import JsonSchemaMixin
 
 from neurosym.bashtool import BashTool
+from neurosym.z3tool import Z3Tool
 from langchain_community.tools import ReadFileTool, WriteFileTool
 
 from .blocks import compute
@@ -18,14 +19,14 @@ class Formula(JsonSchemaMixin):
 
 
 def main():
-    """Simple neurosymbolic solver with bash/read/write file capabilities."""
+    """Simple neurosymbolic solver with bash/z3/read/write file capabilities."""
     if len(sys.argv) < 2:
         print("Usage: You need to provide an instruction to the neurosymbolic solver.")
         sys.exit(1)
 
     user_input = sys.argv[1]
 
-    toolbox = [BashTool(), ReadFileTool(verbose=True), WriteFileTool(verbose=True)]
+    toolbox = [BashTool(), Z3Tool(), ReadFileTool(verbose=True), WriteFileTool(verbose=True)]
     formula, messages = compute(prompt=user_input, toolbox=toolbox, schema=Formula)
     for message in messages:
         print(message)
